4ea83d89b63a64903ff9752d3e15338cc408e557
Paulo Henrique <heypaulim@gmail.com>
2024-07-17 12:22:22 -0300

    conectando mongodb e api

---
+++ src/app.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import conectaNaDatabase from './config/dbConnect.js';
+
+const conexao = await conectaNaDatabase()
+
+conexao.on("error", (erro)=>{
+    console.error('erro de conexão', erro);
+})
+
+conexao.once('open', ()=>{
+    console.log('Conexão com o banco feita com sucesso');
+})
+
+const app = express();
+app.use(express.json())
+
+const livros = [
+    {
+        id: 1,
+        titulo: 'O Senhor dos Anéis'
+    },
+    {
+        id: 2,
+        titulo: 'O Hobbit'
+    }
+]
+
+function buscaLivros(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id)
+    })
+} 
+
+app.get('/', (req,res) => {
+    res.status(200).send('Curso de NodeJS');
+});
+
+app.get('/livros', (req,res) => {
+    res.status(200).json(livros)
+});
+
+app.get('/livros/:id', (req,res)=> {
+    const index = buscaLivros(req.params.id);
+    res.status(200).json(livros[index])
+})
+
+app.post('/livros', (req,res) => {
+    livros.push(req.body)
+    res.status(201).send('Livro cadastrado com sucesso')
+})
+
+app.put('/livros/:id', (req,res) => {
+    const index = buscaLivros(req.params.id);
+    livros[index].titulo = req.body.titulo
+    res.status(200).json(livros)
+})
+
+app.delete('/livros/:id', (req,res) => {
+    const index = buscaLivros(req.params.id);
+    livros.splice(index, 1)
+    res.status(200).send("Livro removido com sucesso");
+});
+
+
+
+
+export default app;
